536ffe39d901997deacdd02707923e966a4a07a8
gusyaraaa <gusyaraaa@yandex.ru>
2024-04-03 15:42:35 +0200

    update netlify config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig, splitVendorChunkPlugin } from "vite";
-import path from "path";
-import react from "@vitejs/plugin-react";
-import svgr from "vite-plugin-svgr";
-import tsconfigPaths from "vite-tsconfig-paths";
-import eslint from "vite-plugin-eslint";
-import checker from "vite-plugin-checker";
-import pluginRewriteAll from "vite-plugin-rewrite-all";
-import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
+import { defineConfig, splitVendorChunkPlugin } from 'vite'
+import path from 'path'
+import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import eslint from 'vite-plugin-eslint'
+import checker from 'vite-plugin-checker'
+import pluginRewriteAll from 'vite-plugin-rewrite-all'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => ({
@@ -26,18 +26,18 @@ export default defineConfig(({ command }) => ({
     port: 3000,
   },
   build: {
-    outDir: "build",
-    assetsDir: "app-assets",
+    outDir: 'build',
+    assetsDir: 'app-assets',
   },
   resolve: {
     alias: {
-      $fonts: path.resolve(__dirname, "src/assets/fonts"),
+      $fonts: path.resolve(__dirname, 'src/assets/fonts'),
     },
   },
   css: {
     modules: {
       generateScopedName:
-        command === "build" ? undefined : "[name]__[local]___[hash:base64:5]",
+        command === 'build' ? undefined : '[name]__[local]___[hash:base64:5]',
     },
     preprocessorOptions: {
       scss: {
@@ -45,4 +45,4 @@ export default defineConfig(({ command }) => ({
       },
     },
   },
-}));
+}))
